433bc684c37c9781f5d2ec0aa4e8ceb8630c9ce9
zahraaltan <zahraaltan783@gmail.com>
2024-12-11 19:16:25 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-document.getElementById('book-now').addEventListener('click', function () {
+document.getElementById('book-now').addEventListener('click', function (e) {
+  e.preventDefault(); // Prevent default button behavior
   const form = document.getElementById('booking-form');
   if (form.checkValidity()) {
     window.location.href = 'redirect.html';
-  } 
+  } else {
+    alert('Please fill out all fields correctly.');
+  }
 });
